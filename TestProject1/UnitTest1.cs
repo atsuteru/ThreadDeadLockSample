022e64efd0f88c70ae96e98f01c2df14e2a8b0a8
KAMIYA.Atsuteru <a-kamiya@pscsrv.co.jp>
2021-08-07 22:52:15 +0900

    スレッドの動きを確認するテストを追加

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -39,12 +39,6 @@ namespace TestProject1
             {
                 Thread.Sleep(10);
             }
-            scheduler.Dispatcher.ShutdownStarted += (s, e) =>
-            {
-            };
-            scheduler.Dispatcher.ShutdownFinished += (s, e) =>
-            {
-            };
             return scheduler;
         }
 
@@ -135,14 +129,147 @@ namespace TestProject1
                 }
             }
 
-            // 先に非同期集計処理が走っている
+            // 先に非同期処理が走っている状態を作る
             ProcessStart();
-
-            // 先の非同期集計処理が終わる前に同期集計処理が呼ばれる
+            // 先の非同期処理が終わる前に同期処理を呼ぶ
             ProcessRun();
-
             // 全ての完了を待つ
             WaitAllTasks();
+        }
+
+        /// <summary>
+        /// ConfigureAwait(false)を使う場合の実行スレッドの違い・・・待った後は別スレッドで継続される。
+        /// </summary>
+        /// <remarks>
+        /// Case: await taskSyncLock.WaitAsync().ConfigureAwait(false);
+        ///   [9]Schedule 1 start.
+        ///   [9]ProcessAsync called. waiting start...
+        ///   [9]ProcessAsync start
+        ///   [9]await start
+        ///   [9]Schedule 1 end.
+        ///   [9]Schedule 2 start.
+        ///   [9]ProcessAsync called. waiting start...
+        ///   [31]await end
+        ///   [31]ProcessAsync end
+        ///   [31]ProcessAsync start ★ワーカースレッドで実行されている
+        ///   [31]await start
+        ///   [10]await end
+        ///   [10]ProcessAsync end
+        ///   [9]WaitAllTasks start
+        ///   [9]WaitAllTasks end
+        ///   [9]Schedule 2 end.
+        ///   [9]Schedule 3 start.
+        ///   [9]Schedule 3 end.
+        /// Case: taskSyncLock.Wait()・・・待った後は元のスレッドで継続される。
+        ///   [9]Schedule 1 start.
+        ///   [9]ProcessAsync called. waiting start...
+        ///   [9]ProcessAsync start
+        ///   [9]await start
+        ///   [9]Schedule 1 end.
+        ///   [9]Schedule 2 start.
+        ///   [9]ProcessAsync called. waiting start...
+        ///   [10]await end
+        ///   [10]ProcessAsync end
+        ///   [9]ProcessAsync start ★Managerスレッドで実行されている
+        ///   [9]await start
+        ///   [31]await end
+        ///   [31]ProcessAsync end
+        ///   [9]WaitAllTasks start
+        ///   [9]WaitAllTasks end
+        ///   [9]Schedule 2 end.
+        ///   [9]Schedule 3 start.
+        ///   [9]Schedule 3 end.
+        /// </remarks>
+        [Fact]
+        public void Test3()
+        {
+            var taskSyncLock = new SemaphoreSlim(1, 1);
+            var runningTasks = new ConcurrentDictionary<int, Task>();
+
+            Task ProcessStart()
+            {
+                return ProcessAsync();
+            }
+
+            void ProcessRun()
+            {
+                ProcessAsync().Wait();
+            }
+
+            async Task ProcessAsync()
+            {
+                TestOutputHelper.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]ProcessAsync called. waiting start...");
+                await taskSyncLock.WaitAsync().ConfigureAwait(false);
+                //taskSyncLock.Wait();
+                TestOutputHelper.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]ProcessAsync start");
+                try
+                {
+                    var task = Task
+                        .Run(() => Thread.Sleep(3000))
+                        .ContinueWith(t =>
+                        {
+                            runningTasks.TryRemove(t.Id, out var removed);
+                        });
+                    runningTasks.TryAdd(task.Id, task);
+                    TestOutputHelper.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]await start");
+                    await task.ConfigureAwait(false);
+                    TestOutputHelper.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]await end");
+                }
+                finally
+                {
+                    TestOutputHelper.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]ProcessAsync end");
+                    taskSyncLock.Release();
+                }
+            }
+
+            void WaitAllTasks()
+            {
+                taskSyncLock.Wait();
+                TestOutputHelper.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]WaitAllTasks start");
+                try
+                {
+                    Task.WaitAll(runningTasks.Values.ToArray());
+                }
+                finally
+                {
+                    TestOutputHelper.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]WaitAllTasks end");
+                    taskSyncLock.Release();
+                }
+            }
+
+            var isEnd = false;
+            DispatcherScheduler managerScheduler = RunExtendableSchedulableThread("Manager");
+
+            managerScheduler.Schedule(() =>
+            {
+                TestOutputHelper.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]Schedule 1 start.");
+                // 先に非同期処理が走る
+                ProcessStart();
+                TestOutputHelper.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]Schedule 1 end.");
+            });
+
+            managerScheduler.Schedule(() =>
+            {
+                TestOutputHelper.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]Schedule 2 start.");
+                // 先の非同期処理が終わる前に同期処理が走る
+                ProcessRun();
+                // 全ての完了を待つ
+                WaitAllTasks();
+
+                isEnd = true;
+                TestOutputHelper.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]Schedule 2 end.");
+            });
+
+            managerScheduler.Schedule(() =>
+            {
+                TestOutputHelper.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]Schedule 3 start.");
+                TestOutputHelper.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]Schedule 3 end.");
+            });
+
+            while (!isEnd)
+            {
+                Thread.Sleep(20);
+            }
         }
     }
 }
